422dc371751a0de4193d1a8318fe0b20f31c3ab0
Diksha Mehta <dikshamehta@Dikshas-MacBook-Air-2.local>
2024-02-27 19:38:42 -0500

    Initial Commit

---
+++ ciphers_project/api/ciphers.py
@@ -0,0 +1,13 @@
+def caesar_encode(plain_text, shift):
+    cipher_text = ""
+    for c in plain_text:
+        if c.isalpha():
+            if c.islower():
+                c_encoded = ord('a') + (ord(c) - ord('a') + shift) % 26
+                cipher_text += chr(c_encoded)
+            elif c.isupper():
+                c_encoded = ord('A') + (ord(c) - ord('A') + shift) % 26
+                cipher_text += chr(c_encoded)
+        else:
+            cipher_text += c
+    return cipher_text
