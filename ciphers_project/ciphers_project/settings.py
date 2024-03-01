bfb1a1de1f1d104adffb873525b723dd0afa3993
adityapadgal <53577378+adityapadgal@users.noreply.github.com>
2024-02-29 21:37:53 -0500

    Update settings.py

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://adityapadgal.io",
+    "https://adityapadgal.github.io",
     "http://localhost:3000"
 ]
