7e778877c77af26f46538971f98ff070ce97f4f7
Aditya Padgal <adityapadgal@Adityas-MacBook-Air-2.local>
2024-02-29 21:31:37 -0500

    Updated Final Changes

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g@vll^f#x#7-#w^@ue+v&f5(x(#3gc^u!@q(#8y3$ibt9jdum^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cipher-service-adityapadgal.onrender.com']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'cipher-service-adityapadgal.onrender.com']
 
 
 # Application definition
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://adityapadgal.io",
+    "http://localhost:3000"
+]
